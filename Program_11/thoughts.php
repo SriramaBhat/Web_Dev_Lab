d71bb64d30b7f5d33dd84101a9abd2a9e47f8dcc
Srirama Bhat <108423390+SriramaBhat@users.noreply.github.com>
2023-06-17 07:46:24 +0530

    Create thoughts.php

---
+++ Program_11/thoughts.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Thoughts</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <h1>Enter your thoughts</h1>
+  <form action="thoughts.php" method="POST">
+    <label for="thoughts">Thoughts:</label>
+    <div id="text-con">
+      <textarea rows="25" cols="100" id="thoughts" name="thoughts"></textarea>      
+    </div>
+    <div id="btn-div">
+      <button type="submit">Submit</button>
+      <button type="reset">Reset</button>
+    </div>
+  </form>
+
+  <?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $thoughts = $_POST["thoughts"];
+      $thought_file = fopen("My_thoughts.txt", "w");
+      fwrite($thought_file, $thoughts);
+      fclose($thought_file);
+      echo "<p>The thought is: </p>";
+      echo "<p class='bold'>$thoughts</p>";
+      $file_path = realpath("My_thoughts.txt");
+      echo "<p>The thought file path is: <span class='bold'>$file_path</span></p>";
+    }
+  ?>
+</body>
+</html>
